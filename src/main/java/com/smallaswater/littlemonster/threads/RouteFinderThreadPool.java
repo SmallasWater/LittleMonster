d28de3132068ff3ee34bd4859771db773c9d83db
LT_Name <572413378@qq.com>
2022-01-31 10:36:54 +0800

    整理下线程池类

--- src/main/java/com/smallaswater/littlemonster/threads/RouteFinderThreadPool.java
+++ src/main/java/com/smallaswater/littlemonster/threads/RouteFinderThreadPool.java
@@ -8,20 +8,32 @@ import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
 import com.smallaswater.littlemonster.threads.runnables.RouteFinderSearchTask;
 
 public class RouteFinderThreadPool {
-   public static ThreadPoolExecutor executor;
+
+   private static final ThreadPoolExecutor executor;
+
+   static {
+      executor = new ThreadPoolExecutor(
+              1,
+              Runtime.getRuntime().availableProcessors() + 1,
+              1L,
+              TimeUnit.SECONDS,
+              new LinkedBlockingQueue<>(),
+              new AbortPolicy()
+      );
+   }
+
+   private RouteFinderThreadPool() {
+      throw new RuntimeException();
+   }
 
    public static void executeRouteFinderThread(RouteFinderSearchTask t) {
       if (!executor.isShutdown() && !executor.isTerminating()) {
          executor.execute(t);
       }
-
    }
 
    public static void shutDownNow() {
       executor.shutdownNow();
    }
 
-   static {
-      executor = new ThreadPoolExecutor(1, Runtime.getRuntime().availableProcessors() + 1, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue(), new AbortPolicy());
-   }
 }
