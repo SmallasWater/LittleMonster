dcd27b7f72a929642d86043c5cd643627b3acc5f
LT_Name <572413378@qq.com>
2023-03-31 20:52:11 +0800

    补充缺少的判断

--- src/main/java/com/smallaswater/littlemonster/entity/baselib/BaseEntityMove.java
+++ src/main/java/com/smallaswater/littlemonster/entity/baselib/BaseEntityMove.java
@@ -144,7 +144,7 @@ public abstract class BaseEntityMove extends BaseEntity {
                     //近战模式忽略部分会飞的实体 防止乱跑
                     //触发事件模式无法确定插件是近战还是远程 当作近战处理
                     //TODO 使用权重功能处理飞行生物，降低飞行生物目标权重
-                    if (this.attactMode == ATTACK_MODE_MELEE) {
+                    if (this.attactMode == ATTACK_MODE_MELEE || this.attactMode == ATTACK_MODE_EVENT) {
                         //忽略蝙蝠 鹦鹉
                         if (entity.getNetworkId() == EntityBat.NETWORK_ID || entity.getNetworkId() == EntityParrot.NETWORK_ID) {
                             continue;
